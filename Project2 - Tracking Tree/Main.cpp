7d410d7ab8a521b38b4d84798bb4a58c69564a1c
Phyntar <43153541+Phyntar@users.noreply.github.com>
2018-11-30 18:23:58 -0700

    Update Main.cpp

--- Project2 - Tracking Tree/Main.cpp
+++ Project2 - Tracking Tree/Main.cpp
@@ -2,16 +2,91 @@
 
 #include "TrackingTree.h"
 
-
 int main()
 {
+	struct Node* root = newNode(10);
+	root->left = newNode(11);
+	root->left->left = newNode(7);
+	root->right = newNode(9);
+	root->right->left = newNode(15);
+	root->right->right = newNode(8);
 
-}
+	cout << "Inorder traversal before insertion:";
+	inorder(root);
 
-int hashed(string in)
-{
+	int key = 12;
+	insert(root, key);
+
+	cout << endl;
+	cout << "Inorder traversal after insertion:";
+	inorder(root);
 
 	return 0;
 }
 
+int hashed(string in)
+{
+	return 0;
+}
 
+
+
+
+
+
+//insert function below
+//https://www.geeksforgeeks.org/insertion-in-a-binary-tree-in-level-order/
+
+struct Node
+{
+	int key;
+	struct Node* left, *right;
+};
+
+
+struct Node* newNode(int key)
+{
+	struct Node* temp = new Node;
+	temp->key = key;
+	temp->left = temp->right = NULL;
+	return temp;
+};
+
+/* Inorder traversal of a binary tree*/
+void inorder(struct Node* temp)
+{
+	if (!temp)
+		return;
+
+	inorder(temp->left);
+	cout << temp->key << " ";
+	inorder(temp->right);
+}
+
+/*function to insert element in binary tree */
+void insert(struct Node* temp, int key)
+{
+	queue<struct Node*> q;
+	q.push(temp);
+
+	// Do level order traversal until we find 
+	// an empty place.  
+	while (!q.empty()) {
+		struct Node* temp = q.front();
+		q.pop();
+
+		if (!temp->left) {
+			temp->left = newNode(key);
+			break;
+		}
+		else
+			q.push(temp->left);
+
+		if (!temp->right) {
+			temp->right = newNode(key);
+			break;
+		}
+		else
+			q.push(temp->right);
+	}
+}
