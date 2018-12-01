d1a05c2ddc47e66def9caafcdf8adfeaf437bc52
mattmcmenamin <43154137+mattmcmenamin@users.noreply.github.com>
2018-11-30 17:56:16 -0700

    Update Main.cpp

--- Project2 - Tracking Tree/Main.cpp
+++ Project2 - Tracking Tree/Main.cpp
@@ -4,21 +4,6 @@
 
 int main()
 {
-
-}
-
-int hashed(string in)
-{
-	return 0;
-}
-
-
-
-
-//code for insertion into the tree
-// Driver code 
-int main()
-{
 	struct Node* root = newNode(10);
 	root->left = newNode(11);
 	root->left->left = newNode(7);
@@ -38,4 +23,72 @@ int main()
 
 	return 0;
 }
+
+int hashed(string in)
+{
+	return 0;
+}
+
+
+
+
+
+
+//insert function below
+//https://www.geeksforgeeks.org/insertion-in-a-binary-tree-in-level-order/
+
+struct Node 
+{
+	int key;
+	struct Node* left, *right;
+};
+
+
+struct Node* newNode(int key)
+{
+	struct Node* temp = new Node;
+	temp->key = key;
+	temp->left = temp->right = NULL;
+	return temp;
+};
+
+/* Inorder traversal of a binary tree*/
+void inorder(struct Node* temp)
+{
+	if (!temp)
+		return;
+
+	inorder(temp->left);
+	cout << temp->key << " ";
+	inorder(temp->right);
+}
+
+/*function to insert element in binary tree */
+void insert(struct Node* temp, int key)
+{
+	queue<struct Node*> q;
+	q.push(temp);
+
+	// Do level order traversal until we find 
+	// an empty place.  
+	while (!q.empty()) {
+		struct Node* temp = q.front();
+		q.pop();
+
+		if (!temp->left) {
+			temp->left = newNode(key);
+			break;
+		}
+		else
+			q.push(temp->left);
+
+		if (!temp->right) {
+			temp->right = newNode(key);
+			break;
+		}
+		else
+			q.push(temp->right);
+	}
+}
+
 //https://www.geeksforgeeks.org/insertion-in-a-binary-tree-in-level-order/
